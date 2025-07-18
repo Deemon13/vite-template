45c1012b5aeb4e47f4178775700e54734a87bf7e
Dmitriy Fedorenko <dmitriy.fedorenko13@gmail.com>
2025-07-19 01:00:59 +0300

    fix: исправляет импорты файла vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
+/// <reference types="vitest" />
+
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 
-/// <reference types="vitest" />
 export default defineConfig({
   base: "/vite-template",
   plugins: [react()],
